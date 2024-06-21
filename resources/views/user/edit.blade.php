efd8b591475f5ae1ba7d5e91588653eec3487421
Kenneth Santiañez <kennethcsantianez@gmail.com>
2024-06-21 13:51:20 +0800

    Add back button

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -67,6 +67,13 @@
 									</svg>	
 									{{ __('Save') }}
 								</x-primary-button>
+								<a href="{{ route('users.index') }}"
+									class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-5">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+									</svg>
+									Back
+								</a>
 							</div>
 						</form>
 					</section>
