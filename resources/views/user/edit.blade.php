ee5edf510de4d229f6eb854a26df1d251520c033
Kenneth Santiañez <kennethcsantianez@gmail.com>
2024-06-21 13:55:41 +0800

    Remove unused code

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -22,10 +22,6 @@
 							</p>
 						</header>
 
-						<form id="send-verification" method="post" action="{{ route('verification.send') }}">
-							@csrf
-						</form>
-
 						<form method="POST" action="{{ route('users.update', $user) }}" class="mt-6 space-y-6">
 							@csrf
 							@method('PATCH')
